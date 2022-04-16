66f2508b1eb588057aa11db43bf988e4262b8074
s-okayasu <40541121+s-okayasu@users.noreply.github.com>
2022-04-17 01:12:27 +0900

    form実装

--- src/app.py
+++ src/app.py
@@ -1,17 +1,23 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
+
+class User:
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
 
 @app.route('/')
 def index():
     return '<h1>Hello World</h1>'
 
-@app.route('/temp/<name>')
-def temp(name):
-    user = {
-        'name': name
-    }
-    return render_template('temp.html', user=user)
+@app.route('/form')
+def temp():
+    user = User(
+        request.args.get('name'),
+        request.args.get('age')
+    )
+    return render_template('form.html', user=user)
 
 if __name__ == '__main__':
     app.run(debug=True)
